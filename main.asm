4d619ddf2b8adbeba5e3fcbc9a5013555cb33f61
Gilles Henrard <gilleshenrard@hotmail.com>
2019-01-02 15:50:00 +0100

    implement main menu logic

--- main.asm
+++ main.asm
@@ -38,8 +38,10 @@ temp_3	    RES 1
 	    
 #define	LED		PORTD
 #define	TRIS_LED	TRISD
-#define	BUTTON1		PORTB
-#define	TRIS_BUTTON1	TRISB
+#define	BUTTON1		PORTB,0
+#define	TRIS_BUTTON1	TRISB,0
+#define	BUTTON2		PORTA,5
+#define	TRIS_BUTTON2	TRISA,5
 
 ;*******************************************************************************
 ;
@@ -102,6 +104,13 @@ stan_table				;table for LCD displays
     movlw   TBL_INTRO2	    ; 
     movwf   ptr_pos	    ;
     call    stan_char_2	    ; send "Dossier 18F8722 " to the LCD line 2
+
+    bcf	    BUTTON1	    ; clear BUTTON1
+    bsf	    TRIS_BUTTON1    ; set PORTB,0 as input
+    movlw   b'00001110'	    ;
+    movwf   ADCON1	    ; set PORTA as digital
+    bcf	    BUTTON2	    ; clear BUTTON2
+    bsf	    TRIS_BUTTON2    ; set PORTA,5 as input
     
     call    delay_1s	    ;
     call    delay_1s	    ; freeze for 5 seconds to display the name 
@@ -205,7 +214,6 @@ d100l1
     
 ;----------------- Main loop ---------------------------------------------------
 main
-    call    ClearLCD	    ; clear the display
     movlw   TBL_MENU_DISPLAY; 
     movwf   ptr_pos	    ;
     call    stan_char_1	    ; send "Affichage" to the LCD line 1
@@ -214,8 +222,59 @@ main
     call    stan_char_2	    ; send "S1:Sel    S2:Svt" to the LCD line 2
     
 menu_display
-    
+    btfsc   BUTTON1
+    ;display menu is selected
+    btfsc   BUTTON2
     goto    menu_display
+    btfss   BUTTON2
+    goto    $-2
+    ;display menu is selected
+    
+    movlw   TBL_MENU_SETTINGS 
+    movwf   ptr_pos	    ;
+    call    stan_char_1	    ; send "Reglage Temps   " to the LCD line 1
+    movlw   TBL_MENU_CHOICE1; 
+    movwf   ptr_pos	    ;
+    call    stan_char_2	    ; send "S1:Sel    S2:Svt" to the LCD line 2
+menu_settings
+    btfsc   BUTTON1
+    ;display menu is selected
+    btfsc   BUTTON2
+    goto    menu_settings
+    btfss   BUTTON2
+    goto    $-2
+    ;display menu is selected
+    
+    movlw   TBL_MENU_CHRONO; 
+    movwf   ptr_pos	    ;
+    call    stan_char_1	    ; send "Chronometre     " to the LCD line 1
+    movlw   TBL_MENU_CHOICE1; 
+    movwf   ptr_pos	    ;
+    call    stan_char_2	    ; send "S1:Sel    S2:Svt" to the LCD line 2
+menu_chrono
+    btfsc   BUTTON1
+    ;display menu is selected
+    btfsc   BUTTON2
+    goto    menu_chrono
+    btfss   BUTTON2
+    goto    $-2
+    ;display menu is selected
+    
+    movlw   TBL_MENU_COUNTDOWN 
+    movwf   ptr_pos	    ;
+    call    stan_char_1	    ; send "Compte à rebours" to the LCD line 1
+    movlw   TBL_MENU_CHOICE1; 
+    movwf   ptr_pos	    ;
+    call    stan_char_2	    ; send "S1:Sel    S2:Svt" to the LCD line 2
+menu_countdown
+    btfsc   BUTTON1
+    ;display menu is selected
+    btfsc   BUTTON2
+    goto    menu_countdown
+    btfss   BUTTON2
+    goto    $-2
+    ;display menu is selected
+    
     GOTO    main	    ; loop forever
 
 ;----------------- End of main program ----------------------------------------
